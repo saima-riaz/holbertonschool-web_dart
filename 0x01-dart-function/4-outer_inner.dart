29d1f550d9688854806586fd3f45f68fe93dbaca
saima-riaz <saimariaz1422@gmail.com>
2025-03-06 18:15:48 +0100

    fixed

--- 0x01-dart-function/4-outer_inner.dart
+++ 0x01-dart-function/4-outer_inner.dart
@@ -1,6 +1,7 @@
 void outer(String name, String id) {
-    String inner() {
-        return "Agent B.$name";
-    }
-    print("Hello ${inner()} your id is $id");
+  String inner() {
+    return "Agent B.$name";
+  }
+  
+  print("Hello ${inner()} your id is $id");
 }
