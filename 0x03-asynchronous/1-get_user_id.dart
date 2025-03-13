bcc9ef309af1b7e6695c597b396424bd6e08e6a1
saima-riaz <saimariaz1422@gmail.com>
2025-03-13 16:09:24 +0100

    fixed

--- 0x03-asynchronous/1-get_user_id.dart
+++ 0x03-asynchronous/1-get_user_id.dart
@@ -1,6 +1,6 @@
-import '0-util.dart';
 import 'dart:convert';
+import '1-util.dart';
 
 Future<String> getUserId() {
-    return fetchUserData().then((user) => json.decode(user)['id']);
+  return fetchUserData().then((user) => json.decode(user)['id']);
 }
